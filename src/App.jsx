948815bf318d5e284d2e787b4b5c49c8f6d623db
kevmhughes <kevhughes24@hotmail.com>
2024-12-06 22:21:31 +0100

    Refactor: Add explanation toggle functionality - Implemented the ability to show or hide explanations for each question. - Added a button to toggle explanation visibility and manage its state using `useState`. - Used `useEffect` to reset explanation visibility when the question index changes.

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import {
   handleChange,
   handleNextQuestion,
   handleShowAIQuiz,
-} from "./handlers/formhandlers";
+} from "./handlers/formHandlers"
 import Question from "./components/Question";
-import Form from "./components/Form";
 import Answers from "./components/Answers";
 import Explanation from "./components/Explanation";
+import AiInputChoices from "./components/AiInputChoices";
 import "./assets/styles/styles.css";
 import backUpArray from "../src/utils/backUpArray";
+import Image from "./components/Image";
 
 function App() {
   const [questions, setQuestions] = useState([]);
@@ -29,6 +30,9 @@ function App() {
   const [index, setIndex] = useState(0);
   const [showExplanationButton, setShowExplanationButton] = useState(false);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [AiDb, setAiDb] = useState(false);
+  const [randomDb, setRandomDb] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
   const questionIndex = index;
   const hasFetched = useRef(false); // UseRef to track fetch status
@@ -44,23 +48,29 @@ function App() {
       /* setLoading(true) */ // Ensure loading is set to true before the request
 
       try {
-        // AI GENERATED QUESTION
-        /* const response = await axios.get(
-          `https://codequestapi.onrender.com/api/v1/questions/ai?amount=${formData.amount}&topic=${formData.topic}`
-        );
-        
-        setQuestions(response.data.results); // Update questions state */
+        if (AiDb) {
+          // AI GENERATED QUESTION
+          const response = await axios.get(
+            `https://codequestapi.onrender.com/api/v1/questions/ai?amount=${formData.amount}&topic=${formData.topic}`
+          );
 
+          setQuestions(response.data.results); // Update questions state
+        }
+
+        /* if (randomDb) {
         // RANDOMLY GENERATED QUESTION
-        /* const response = await axios.get(
+        const response = await axios.get(
           `https://codequestapi.onrender.com/api/v1/questions/random?amount=${formData.amount}`
         );
         
-        setQuestions(response.data.results);  */
+        setQuestions(response.data.results); 
+        } */
 
-        // BACK UP QUESTIONS
-        setQuestions(backUpArray[0].results);
-        /* setQuestions(backUpArray[0].results); */
+        if (randomDb) {
+          // BACK UP QUESTIONS
+          setQuestions(backUpArray[0].results);
+        }
+
         setError(null); // Reset error if the request was successful
       } catch (error) {
         setError("There was an error fetching the data.");
@@ -71,7 +81,7 @@ function App() {
     };
 
     fetchQuestions(); // Fetch questions only once on mount
-  }, [formData]);
+  }, [formData, AiDb, randomDb]);
 
   const questionsToDisplay =
     questions.length > 0 ? questions : backUpArray[0].results;
@@ -99,12 +109,18 @@ function App() {
       values,
       setValues,
       hasFetched,
-      setLoading
+      setLoading,
+      setAiDb,
+      setRandomDb
     );
   };
 
   if (loading) {
-    return <div>AI is generating your questions with your settings...</div>;
+    return (
+      <div className="loader-container">
+        <Image alt="CodeBrain app logo rotating during loading" className="rotating-image" />
+      </div>
+    );
   }
 
   // If no questions are available (both fetch failed and backup is empty), show a fallback message
@@ -119,50 +135,116 @@ function App() {
     return <div>Testing</div>; /* <div>{error}</div>; */
   }
 
+  const handleRandomDb = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setRandomDb(true);
+    setShowQuizPage(true);
+    setFormData(values);
+    setValues({
+      amount: 1,
+    });
+    hasFetched.current = false;
+    setAiDb(false);
+  };
+
+  const handleShowForm = () => {
+    setShowForm(true);
+  };
+
+  const handleReturnToStart = () => {
+    setShowQuizPage(false);
+    setShowForm(false);
+  };
+
   return (
-    <div className="main-container">
-      <h1>Quiz Master</h1>
-      {showQuizPage ? (
-        <>
-          <h1>
-            Question: {questionIndex + 1}/{questionsToDisplay.length}
-          </h1>
-          <Question questions={questionsToDisplay} index={index} />
-          <Answers
-            questions={questionsToDisplay}
-            handleNextQuestion={nextQuestion}
-            setShowExplanationButton={setShowExplanationButton}
-            setSelectedAnswer={setSelectedAnswer}
-            selectedAnswer={selectedAnswer}
-            index={index}
-          />
-          {showExplanationButton && (
-            <Explanation questions={questionsToDisplay} index={index} />
-          )}
-          <br />
-          {index + 1 === questions.length ? (
-            <Form handleSubmit={submit} handleChange={change} values={values} />
-          ) : (
-            <div></div>
-          )}
-        </>
-      ) : (
-        <div>
-          <div>
-            Choose A Quiz:
-            <Form
-              handleSubmit={submit}
-              handleChange={change}
-              handleShowAIQuiz={showAIQuiz}
-              values={values}
+    <>
+      <header>
+        <Image
+          className="header-logo"
+          alt="Logo of the CodeBrain app in the header"
+          handleReturnToStart={handleReturnToStart}
+        />
+      </header>
+      <div className="main-container">
+        <h1>&lt;CodeBrain&gt;</h1>
+        {showQuizPage ? (
+          <div className="quiz-container">
+            <h1>
+              Question: {questionIndex + 1}/{questionsToDisplay.length}
+            </h1>
+            <Question questions={questionsToDisplay} index={index} />
+            <Answers
+              questions={questionsToDisplay}
+              handleNextQuestion={nextQuestion}
+              setShowExplanationButton={setShowExplanationButton}
+              setSelectedAnswer={setSelectedAnswer}
+              selectedAnswer={selectedAnswer}
+              index={index}
             />
+            {showExplanationButton && (
+              <Explanation questions={questionsToDisplay} index={index} />
+            )}
+            <br />
+            {index + 1 === questions.length ? (
+              <button onClick={handleReturnToStart} style={{ width: "100%" }}>
+                See Your Score
+              </button>
+            ) : (
+              <div></div>
+            )}
           </div>
-          <button style={{ width: "100%", marginTop: "1rem" }}>
-            Random Generator
-          </button>
-        </div>
-      )}
-    </div>
+        ) : (
+          <div className="choices-container">
+            <Image alt="Logo of the CodeBrain app"/>
+            <div className="AI-input-choices-container">
+              <div>
+                {showForm ? (
+                  <AiInputChoices
+                    handleShowAIQuiz={showAIQuiz}
+                    handleSubmit={submit}
+                    handleChange={change}
+                    values={values}
+                    showForm={showForm}
+                  />
+                ) : (
+                  <div></div>
+                )}
+              </div>
+            </div>
+            {showForm ? (
+              <div></div>
+            ) : (
+              <>
+                <div style={{ marginTop: "3rem" }}>Choose A Quiz:</div>
+                <div className="start-page-buttons-container">
+                  <button
+                    style={{
+                      width: "100%",
+                      marginTop: "1rem",
+                      backgroundColor: "#0097B2",
+                    }}
+                    onClick={handleShowForm}
+                  >
+                    AI
+                  </button>
+                  <button
+                    style={{
+                      width: "100%",
+                      marginTop: "1rem",
+                      backgroundColor: "#004AAD",
+                    }}
+                    onClick={handleRandomDb}
+                  >
+                    CB Quiz
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
