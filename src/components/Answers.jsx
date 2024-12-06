42c1d7beeed6d8efa2931da313f70b50c2c65ee3
kevmhughes <kevhughes24@hotmail.com>
2024-12-06 18:40:01 +0100

    Refactor: Shuffle answers on load and disable buttons after selection
    - Implemented Fisher-Yates shuffle to randomize the order of answers when the question is displayed.
    - Disabled answer buttons once an answer is selected to prevent multiple selections.
    - Displayed "Next Question" button only after an answer is selected, and only if there are more questions available.

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,18 +1,67 @@
 /* eslint-disable */
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { fisherYatesShuffle } from "../utils/fisher-yates-shuffle";
 
-const Answers = ({ questions, handleNextQuestion, index }) => {
-    console.log("question", questions)
+const Answers = ({
+  questions,
+  handleNextQuestion,
+  index,
+  setIndex,
+  setShowExplanationButton,
+  setSelectedAnswer,
+  selectedAnswer,
+}) => {
+  // Function to handle when an answer is selected
+  const handleAnswerClick = (answer) => {
+    setSelectedAnswer(answer); // Store the selected answer
+    setShowExplanationButton(true); // Show the explanation button
+  };
+
+  // Shuffling answers on initial render (not on every re-render)
+  const [shuffledAnswers, setShuffledAnswers] = useState([]);
+
+  useEffect(() => {
+    // Shuffle answers when the component is mounted or when the question changes
+    setShuffledAnswers(fisherYatesShuffle(questions[index].answerOptions));
+  }, [index, questions]);
+
   return (
     <div className="answers-container">
-    {questions[index].answerOptions.map((answer, answerIndex) => (
-      <button key={answerIndex}>
-        {answer.answer} <b>{answer.isCorrect ? "correct" : "incorrect"}</b>
-      </button>
-    ))}
-    <button onClick={handleNextQuestion}>Next Question</button>
-  </div>
-  )
-}
+      {shuffledAnswers.map((answer, answerIndex) => {
+        const isCorrect = answer.isCorrect;
+        let buttonStyle = {};
 
-export default Answers
+        // Determine the color based on whether the answer is correct or not
+        if (selectedAnswer === answer) {
+          buttonStyle = {
+            backgroundColor: isCorrect ? "green" : "red",
+            color: "white",
+          };
+        }
+
+        return (
+          <button
+            key={answerIndex}
+            onClick={() => handleAnswerClick(answer)}
+            style={buttonStyle}
+            disabled={selectedAnswer !== null}
+          >
+            {answer.answer}
+          </button>
+        );
+      })}
+
+      {selectedAnswer && questions.length !== index + 1 ? (
+        <button
+          onClick={() => handleNextQuestion(setIndex, setShowExplanationButton)}
+        >
+          Next Question
+        </button>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
+
+export default Answers;
