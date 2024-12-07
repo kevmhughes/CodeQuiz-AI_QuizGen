34a581553106006ea41825723037330512e0c348
kevmhughes <kevhughes24@hotmail.com>
2024-12-07 14:43:30 +0100

    Add styles to various components for improved UI

--- src/components/GeneratorButton.jsx
+++ src/components/GeneratorButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const GeneratorButton = ({ handleShowAIQuiz }) => {
   return (
-    <button style={{ width: "100%" }} onClick={handleShowAIQuiz}>
+    <button style={{ width: "100%", backgroundColor: "#0097B2" }} onClick={handleShowAIQuiz}>
       AI Generator
     </button>
   );
