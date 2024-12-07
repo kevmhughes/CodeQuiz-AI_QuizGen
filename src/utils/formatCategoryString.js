186e9ee9d4b29067c5d3cf2db768a610031a74fa
kevmhughes <kevhughes24@hotmail.com>
2024-12-07 20:02:03 +0100

    Add categoryString function to map topic codes to user-friendly names

---
+++ src/utils/formatCategoryString.js
@@ -0,0 +1,39 @@
+export const categoryString = (topic) => {
+  if (topic === "" || undefined) {
+    return (topic = "Random");
+  } else if (topic === "angular") {
+    return (topic = "Angular");
+  } else if (topic === "cloudcomputing") {
+    return (topic = "Cloud Computing");
+  } else if (topic === "css") {
+    return (topic = "CSS");
+  } else if (topic === "datasecurity") {
+    return (topic = "Data Security");
+  } else if (topic === "dom") {
+    return (topic = "DOM");
+  } else if (topic === "express") {
+    return (topic = "Express");
+  } else if (topic === "html") {
+    return (topic = "HTML");
+  } else if (topic === "javascript-advanced") {
+    return (topic = "JavaScript - Advanced");
+  } else if (topic === "javascript-basics") {
+    return (topic = "JavaScript - Basics");
+  } else if (topic === "mongodb") {
+    return (topic = "MongoDB");
+  } else if (topic === "python") {
+    return (topic = "Python");
+  } else if (topic === "react") {
+    return (topic = "React");
+  } else if (topic === "testing") {
+    return (topic = "Testing");
+  } else if (topic === "sql") {
+    return (topic = "SQL");
+  } else if (topic === "typescript") {
+    return (topic = "TypeScript");
+  } else if (topic === "vue") {
+    return (topic = "Vue");
+  }
+};
+
+export default categoryString;
