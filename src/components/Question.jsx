7a092abf14dbb2fbb5b51c461dcfb5983d157539
kevmhughes <kevhughes24@hotmail.com>
2024-12-10 02:02:28 +0100

    feat: pass AiDb and randomDb props to Question component for dynamic content rendering
    - Integrated AiDb and randomDb props to conditionally render code examples and markdown content
    - Enhanced rendering logic for code snippets and markdown based on data source (AI or random)

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,9 +1,58 @@
 /* eslint-disable */
-
 import React from "react";
+import ReactMarkdown from "react-markdown";
+import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+/* import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs"; */
 
-const Question = ({ questions, index }) => {
-  return <div className="question-container">{questions[index].question}</div>;
+const Question = ({ questions, index, AiDb, randomDb }) => {
+  const questionData = questions[index];
+  const codeSnippets = questionData.codeExamples;
+  const markdownContent = questions[index].codeExamples;
+
+  return (
+    <>
+      <div className="question-container">{questions[index].question}</div>
+      {/* Code exmples and answers below */}
+      {/* When ferching from questions/random endpoint */}
+      {randomDb && codeSnippets && codeSnippets.length > 0 && (
+        <div className="react-markdown-wrapper">
+          {codeSnippets.map((snippet, idx) => (
+            <div key={idx} className="code-example-container">
+              <SyntaxHighlighter language="javascript" style={docco}>
+                {snippet}
+              </SyntaxHighlighter>
+            </div>
+          ))}
+        </div>
+      )}
+      {/* When fetching from questions/ai endpoint */}
+      {AiDb && (
+        <div className="react-markdown-wrapper">
+          <ReactMarkdown
+            children={markdownContent}
+            components={{
+              code({ node, inline, className, children, ...props }) {
+                const language = className.replace("language-", "");
+                return !inline ? (
+                  <SyntaxHighlighter
+                    language={language}
+                    style={docco}
+                    {...props}
+                  >
+                    {String(children).replace(/\n$/, "")}
+                  </SyntaxHighlighter>
+                ) : (
+                  <code {...props}>{children}</code>
+                );
+              },
+            }}
+          />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Question;
